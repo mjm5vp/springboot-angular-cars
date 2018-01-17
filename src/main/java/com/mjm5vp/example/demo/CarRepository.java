8949c7bcb9819532b3e12baa4207768443806875
Mark Moeller <MarkMoellerUVA@gmail.com>
2018-01-17 14:49:46 -0500

    functional styled working CRUD

--- src/main/java/com/mjm5vp/example/demo/CarRepository.java
+++ src/main/java/com/mjm5vp/example/demo/CarRepository.java
@@ -2,7 +2,9 @@ package com.mjm5vp.example.demo;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 @RepositoryRestResource
+@CrossOrigin(origins = "http://localhost:4201")
 interface CarRepository extends JpaRepository<Car, Long> {
 }
--- src/main/java/com/mjm5vp/example/demo/CoolCarController.java
+++ src/main/java/com/mjm5vp/example/demo/CoolCarController.java
@@ -2,6 +2,7 @@ package com.mjm5vp.example.demo;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import java.util.Collection;
 import java.util.stream.Collectors;
 
@@ -14,6 +15,7 @@ class CoolCarController {
     }
 
     @GetMapping("/cool-cars")
+    @CrossOrigin(origins = "http://localhost:4201")
     public Collection<Car> coolCars() {
         return repository.findAll().stream()
                 .filter(this::isCool)
