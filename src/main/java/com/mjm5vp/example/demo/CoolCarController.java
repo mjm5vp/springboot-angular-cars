2c1ab348648b0a280b6b2a31409cdb71d69067d3
Mark Moeller <MarkMoellerUVA@gmail.com>
2018-01-19 13:12:36 -0500

    changed client port to 4200

--- src/main/java/com/mjm5vp/example/demo/CoolCarController.java
+++ src/main/java/com/mjm5vp/example/demo/CoolCarController.java
@@ -15,7 +15,7 @@ class CoolCarController {
     }
 
     @GetMapping("/cool-cars")
-    @CrossOrigin(origins = "http://localhost:4201")
+    @CrossOrigin(origins = "http://localhost:4200")
     public Collection<Car> coolCars() {
         return repository.findAll().stream()
                 .filter(this::isCool)
